2374d9e90b0669f2c7bd6293e4e3923a59ee3d72
Anmol <anmol@qdesq.com>
2024-01-31 21:44:59 -0500

    Initial Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.mapd721_a1_anmolsharma"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.mapd721_a1_anmolsharma"
